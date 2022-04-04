af58aff02f9c56d7fc7b9cd9834b1b9c0b9e1ae6
Shaheem <77206224+TheGoodEngineer@users.noreply.github.com>
2022-04-04 15:06:20 +0200

    Point Cloud Generated

--- Perception/laser_Stripe_Sensor_Calibration_and_PointCloudGeneration/laserLine_calibration_point_cloud_generation.m
+++ Perception/laser_Stripe_Sensor_Calibration_and_PointCloudGeneration/laserLine_calibration_point_cloud_generation.m
@@ -9,16 +9,16 @@ close all; clearvars; clc;
 folder = fileparts(which(mfilename));                           
 addpath(genpath(folder));
 
+% Data 
+Cloud = [];
 
-    
 for rot = 1:360
-%% Load Data
+    
     im = imread(sprintf('%d.jpg',rot));
-    %im = imread('1.jpg');
     imRot = imrotate(im, 90);
     imgr = rgb2gray(imRot);
 
-    %% Line Fitting using Max (pixel accuracy)
+    %% using Max (pixel accuracy)
     count = 1;
     [nRows, nColumns] = size(imgr);
     %figure; imshow(imRot);
@@ -35,7 +35,7 @@ for rot = 1:360
         end
     end 
 
-    %% Line Crossing
+    %% Line Fitting
 
     % Manual Limits
     xLimits = [639, 568, 562, 544, 534, 515, 509, 488, 483,  465,  459, 329, 328]; % 329, 328];
@@ -43,15 +43,20 @@ for rot = 1:360
 
     %figure; imshow(imRot); hold on;
 
-    for i = 1:length(xLimits)-1
+    for i = 1:length(xLimits)-2
+        
+        e = 5;
+        if rot == 255 || rot == 302 || rot == 306
+            e = 7;
+        end
         
         diff_x1 = abs(x - xLimits(i));
         diff_y1 = abs(y - yLimits(i));
         diff_x2 = abs(x - xLimits(i + 1));
         diff_y2 = abs(y - yLimits(i + 1));
 
-        stepInit = (diff_x1 < 7) & (diff_y1 < 7);
-        stepEnd = (diff_x2 < 7) & (diff_y2 < 7);
+        stepInit = (diff_x1 < e) & (diff_y1 < e);
+        stepEnd = (diff_x2 < e) & (diff_y2 < e);
 
         idxInit = find(stepInit, 01, 'first');
         idxEnd = find(stepEnd, 01, 'first');
@@ -79,48 +84,70 @@ for rot = 1:360
     %% Intersection of lines
 
     %figure, imshow(imRot); hold on;
-    for i = 1:length(xLimits)-2
+    for i = 1:length(xLimits)-3
+        
         [x_corner(i), y_corner(i)] = polyxpoly(xLine(i, :), yLine(i, :), xLine(i+1, :) , yLine(i+1, :));
+        
         %plot(x_corner(i), y_corner(i), 'b.')
     end
 
     %% Homography calculation
+    
     Pwth = [222, 200, 200, 150, 150, 100, 100, 50, 50, 0;
         -23, -23, -36, -36, -49, -49, -62, -62, -75, -75];
+    
     Pi = [x_corner(1:10); y_corner(1:10)];
     [h, h1, L] = calculateHomo(Pi, Pwth);
     H1 = reshape(h1, 3, 3)';
     H1inv = inv(H1);
 
-    % Project points from image plane to world
+    % image plane to world
     Pi = [Pi; ones(1, size(Pi, 2))];                
     Pw = H1inv * Pi;
     Pw = Pw ./ Pw(3, :);
 
     %% Generate Point Cloud
     Npoints = 100;     
-    CloudPtsTot = [];
-    %figure, imshow(im_rot);
-    hold on;    
+    
+    %figure, imshow(im_rot); hold on;    
+    
     for i = 1 : length(x_corner)-1
         x_line(i, :) = linspace(x_corner(i), x_corner(i+1), Npoints);
         y_line(i, :) = linspace(y_corner(i), y_corner(i+1), Npoints);
 
         %plot(x_line(i, :), y_line(i, :),'b');
     end
-    % Generate point cloud
+    
+    % point cloud
     x_lineT = x_line';
     y_lineT = y_line'; 
     
     for i = 1:length(x_line) * 9  
         CloudPts(i, :) = [x_lineT(i), y_lineT(i), 1];
     end 
+    
     CloudPtsT = CloudPts';
     CloudPts_mm = H1inv * CloudPtsT;
     CloudPts_mm = CloudPts_mm ./ CloudPts_mm(3, :);
-    CloudPtsTot = [CloudPtsTot, CloudPts_mm ];
+    
+    Rx = [1,          0,              0; ...
+       0,        cosd(rot),    sind(rot); ...
+       0,     sind(rot),       cosd(rot)];
+   
+    %Ry = [cosd(rot),          0,              sind(rot); ...
+     %       0,                1,                0; ...
+      %  -sind(rot),           0,             cosd(rot)];
+   
+    % Generate profile (x = 0, y, z)
+    
+    CloudPts_Tot = Rx * CloudPts_mm;
+    
+    Cloud = [Cloud, CloudPts_Tot];
+    
     % Write cloud point
-    writeCloud('PointCloud.txt', CloudPtsTot);
+    %writeCloud('PointCloud_x.txt', ptCloud);
+    writeCloud('PointCloud.txt', Cloud);
+    
     %ptCloud = pointCloud(CloudPtsTot, 'Location');
     %pcshow(ptCloud);
 
